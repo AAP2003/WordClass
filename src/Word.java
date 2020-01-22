58f7cfe44d571c4a4b45445cf9d45986ea76bae9
Aarya Patel <aarya.patel@gmail.com>
2020-01-22 18:40:40 -0500

    Finished Word class

--- src/Word.java
+++ src/Word.java
@@ -1,2 +1,29 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Word {
+	private final String word;
+	private final String[] syllables;
+
+	public Word(String word, String[] syllables) {
+		this.word = word;
+		this.syllables = syllables;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public String[] getSyllables() {
+		return syllables;
+	}
+
+	public int getNumSyllables() {
+		return syllables.length;
+	}
+
+	public String toString() {
+		return "Word: " + word + "\nSyllables: " + Arrays.stream(syllables).map(a -> "-" + a)
+				.collect(Collectors.joining()).substring(1);
+	}
 }
